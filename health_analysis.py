d76aef91efb11adc7cff79f94e97744f89001f4b
Maliha Hossain <102617334+malh718@users.noreply.github.com>
2023-08-18 08:39:30 -0400

    Add files via upload

---
+++ health_analysis.py
@@ -0,0 +1,45 @@
+import pandas as pd 
+import numpy as np
+
+pt_age= 18
+pt_name= 'samantha'
+pt_char= ['6 foot','black hair','brown eyes'] 
+
+provider_dict= {
+    'providerID': [1,2],
+    'providersInfo' : [
+        {
+            'name': 'Dr.Bailey',
+            'speciality': 'surgery'
+        },
+        { 'name': 'Dr. Grey',
+           'speciality' :' general medicine'
+        }
+    ]
+}
+
+
+
+
+def bpChecker(sys, dia):
+    if sys >= 120: 
+        sys_output= 'not good'
+    else: 
+        sys_output='its alright' 
+    if dia >= 80:
+        dia_ouput='not good'
+    else:
+        dia_ouput='its okay'
+    output=[ sys_output,dia_ouput]
+    return output
+
+sys_input= 118
+dia_input= 81 
+
+bp_result= bpChecker(sys_input, dia_input)
+print ('Systolic num: ', sys_input)
+print ('Diastolic num: ',dia_input)
+print ("blood pressure analysis:", bp_result)
+
+
+
